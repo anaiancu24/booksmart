2c6a08601c53635212004ffd6c95d4e29ba955d6
anaiancu24 <ana.iulia24@icloud.com>
2020-11-21 23:44:35 +0100

    Add hardcoded links for each folder item

--- content.js
+++ content.js
@@ -1,38 +1,81 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Load storage data in popup
-
-    chrome.storage.local.get('folderList', function(data){
+    // Load storage data in popup on popup load
+    chrome.storage.local.get('folderList', function (data) {
+        // check if there are any folders already
         if (data.folderList === undefined || data.folderList.length == 0) {
-            chrome.storage.local.set({'folderList':[]});
+            chrome.storage.local.set({ 'folderList': [] });
         } else {
-            for (let i=0; i<data.folderList.length; i++) {
+            // loop through folders are display as list items
+            for (let i = 0; i < data.folderList.length; i++) {
                 let folderElement = document.createElement("li")
                 folderElement.innerHTML = data.folderList[i]
                 document.getElementById('folders-list').appendChild(folderElement)
+                // create an addLink button for each new folder item
+                let addLinkElement = document.createElement("button")
+                addLinkElement.innerHTML = "+"
+                addLinkElement.id = data.folderList[i]
+                folderElement.appendChild(addLinkElement)
+                // create a ul element and append it for each folder item
+                let linksListElement = document.createElement("ul")
+                folderElement.appendChild(linksListElement)
+                // on add Link button click, add a link to the folder item list
+                addLinkElement.addEventListener("click", () => {
+                    let linkElement = document.createElement("a")
+                    // get current tab url
+                    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                        console.log(tabs)
+                        linkElement.innerHTML = tabs[0].title
+                        linkElement.href = tabs[0].url
+                        linkElement.target = "_blank"
+                     });
+                    linksListElement.appendChild(linkElement)
+                })
             }
         }
     })
 
-    addNewFolder = function() {
-        chrome.storage.local.get('folderList', function(data){
-                let newFolderElement = document.createElement("li")
-                lastItemIndex = data.folderList.length - 1
-                newFolderElement.innerHTML = data.folderList[lastItemIndex]
-                document.getElementById('folders-list').appendChild(newFolderElement)
+    // function to display the new folder item
+    addNewFolder = function () {
+        chrome.storage.local.get('folderList', function (data) {
+            // create the folder node element and append it to the list
+            let newFolderElement = document.createElement("li")
+            lastItemIndex = data.folderList.length - 1
+            newFolderElement.innerHTML = data.folderList[lastItemIndex]
+            document.getElementById('folders-list').appendChild(newFolderElement)
+            // create an addLink button for each new folder item
+            let addLinkElement = document.createElement("button")
+            addLinkElement.innerHTML = "+"
+            addLinkElement.id = data.folderList[lastItemIndex]
+            newFolderElement.appendChild(addLinkElement)
+            // create a ul element and append it for each folder item
+            let linksListElement = document.createElement("ul")
+            newFolderElement.appendChild(linksListElement)
+            // on add Link button click, add a link to the folder item list
+            addLinkElement.addEventListener("click", () => {
+                let linkElement = document.createElement("li")
+                linkElement.innerHTML = "Test Link"
+                linksListElement.appendChild(linkElement)
+            })
         })
     }
 
-    //Get input value and store it
+    //Get input value and add it to the folders list on button click
     let button = document.getElementById('addFolderButton')
+    // add onclick event to the addFolder button
     button.addEventListener("click", () => {
-        chrome.storage.local.get('folderList', function(data){
+        chrome.storage.local.get('folderList', function (data) {
+            // get old folders list
             let oldFolderList = data.folderList
+            // get input value and ass it to the old folders list
             let newFolder = document.getElementById("folder-input").value
             oldFolderList.push(newFolder)
-            chrome.storage.local.set({'folderList': oldFolderList});
+            // add the new folders list to the storage
+            chrome.storage.local.set({ 'folderList': oldFolderList });
+            // call the function to display the recent folder
             addNewFolder()
-            document.getElementById("folder-input").value = ""     
+            // clear the input
+            document.getElementById("folder-input").value = ""
         })
     })
-    
-   })
+
+})
