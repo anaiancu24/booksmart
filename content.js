7af265487c5ffdd09575f1f03c179db66500b486
anaiancu24 <ana.iulia24@icloud.com>
2020-11-21 19:41:04 +0100

    Almost done adding folders in list

--- content.js
+++ content.js
@@ -1,20 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
     // Load storage data in popup
-    chrome.storage.local.get('folder', function(data){
-        document.getElementById('storage').innerHTML = data.folder
+
+    chrome.storage.local.get('folders', function(data){
+        if (data.folders === undefined) {
+            chrome.storage.local.set({'folders':[]});
+        }else {
+            for (let i=0; i<data.folders[0].length;i++) {
+                let folderElement = document.createElement("li")
+                folderElement.innerHTML = data.folders[0][i]
+                document.getElementById('folders-list').appendChild(folderElement)
+            }
+        }
     })
 
-    getNewFolders = function() {
-        chrome.storage.local.get('folder', function(data){
-            document.getElementById('storage').innerHTML = data.folder
+    addNewFolder = function() {
+        chrome.storage.local.get('folders', function(data){
+                let newFolderElement = document.createElement("li")
+                lastItemIndex = data.folders[0].length - 1
+                console.log(data.folders[0])
+                console.log(lastItemIndex)
+                newFolderElement.innerHTML = data.folders[0][lastItemIndex]
+                document.getElementById('folders-list').appendChild(newFolderElement)
         })
     }
     // Get input value and store it
     let button = document.getElementById('addFolderButton')
     button.addEventListener("click", () => {
-        let newFolder = document.getElementById("folder-input").value
-        chrome.storage.local.set({'folder':[newFolder]});
-        getNewFolders()
+        chrome.storage.local.get('folders', function(data){
+            let oldFolders = data.folders[0]
+            let newFolder = document.getElementById("folder-input").value
+            oldFolders.push(newFolder)
+            chrome.storage.local.set({'folders':[oldFolders]});
+            addNewFolder()
+            document.getElementById("folder-input").value = ""
+        })
     })
     
    })
